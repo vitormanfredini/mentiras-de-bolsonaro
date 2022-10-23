0d5f298c94fc967ab2a16399ae5a26774ebaf51a
Vítor Manfredini <vitormanfredini@gmail.com>
2022-10-23 20:23:44 -0300

    refatoração

--- run.php
+++ run.php
@@ -2,12 +2,12 @@
 
 //configurações
 $emojiMentiu = '🤥';
-$emojiOk = '✅';
-$colunas = 10;
+$emojiOk = '🟢';
+$colunas = 7;
 
 //1 para explicitar tudo que está fazendo durante o crawler
 //0 para não mostrar nada
-define('DEBUG',0);
+define('DEBUG',1);
 
 include('functions.php');
 
@@ -16,37 +16,28 @@ if(!is_dir('./cache')){
 	mkdir("./cache", 0777);
 }
 
-//tem permissão de escrita?
-// if(!isWritable('./cache')){
-// 	echo 'Pasta ./cache nao tem permissao de escrita.'."\n";
-// 	die();
-// }
-
 //coisas relacionadas ao crawler
 $host = 'www.aosfatos.org';
-$page = 1;
-$strPagePlaceholder = '||PAGE||';
 $urlStart = 'https://www.aosfatos.org/todas-as-declara%C3%A7%C3%B5es-de-bolsonaro/';
-$urlSearchBase = 'https://www.aosfatos.org/todas-as-declara%C3%A7%C3%B5es-de-bolsonaro/?page=||PAGE||';
+$urlSearchBase = 'https://www.aosfatos.org/todas-as-declara%C3%A7%C3%B5es-de-bolsonaro/?page=';
 $countPageResults = 0;
 $countCacheInvalidado = 0;
 $arrOutput = array();
 
-//Baixando dados
 echo 'Baixando dados. Isso vai demorar alguns minutos...'."\n";
 
 //pega página inicial para pegar cookies, etc
 $contents1 = get_web_page($urlStart,$host);
 
 //busca páginas até não encontrar mais fatos
-while(true){
+for($page = 1; true ; $page++){
 
 	//pega página de resultado
-	$urlSearch = str_replace('||PAGE||',$page,$urlSearchBase);
+	$urlSearch = $urlSearchBase.$page;
 	$contents2 = get_web_page($urlSearch,$host,$contents1['cookies'],$urlStart);
 	//se não usou cache, espera um pouco para não sobrecarregar servidor
 	if($contents2['cache'] == false){
-		sleep(rand(1,3));
+		sleep(rand(1,2));
 	}
 	$countPageResults++;
 
@@ -69,15 +60,7 @@ while(true){
 			}
 			$arrItem['title'] = trim(strip_tags($arrParts[1]));
 
-			//extrai texto corrido
-			// $arrParts = explodeByArray(array('neuton fs20 w300">','</p>'),$strFactsPart);
-			// if(!isset($arrParts[1])){
-			// 	throw new Exception('Nao encontrou texto corrido.');
-			// }
-			// $textCorrido = trim(strip_tags($arrParts[1]));
-			// $textCorrido = mb_strtolower($textCorrido);
-
-			//data
+			//extrai data
 			$arrParts = explode('<a',$strFactsPart);
 			if(!isset($arrParts[1])){
 				throw new Exception('Nao encontrou data.');
@@ -93,31 +76,23 @@ while(true){
 			//trata possíveis erros
 			echo 'Erro: '.$e->getMessage()."\n";
 
-			if(false !== strstr($contents2['content'], '<p>Please try again in a few minutes.</p>')){
-				limpaCache($urlSearch,$host,$urlStart);
-				$countCacheInvalidado++;
-			}elseif(false !== strstr($contents2['content'], 'Could not resolve host: www.aosfatos.org')){
-				limpaCache($urlSearch,$host,$urlStart);
-				$countCacheInvalidado++;
-			}elseif(isset($contents2['errmsg']) && false !== strstr($contents2['errmsg'], 'Could not resolve host')){
-				limpaCache($urlSearch,$host,$urlStart);
-				$countCacheInvalidado++;
-			}elseif(isset($contents2['errmsg']) && false !== strstr($contents2['errmsg'], 'handshake')){
-				limpaCache($urlSearch,$host,$urlStart);
-				$countCacheInvalidado++;
-			}elseif(false !== strstr($contents2['content'], 'Web server is returning an unknown error</title>')){
-				echo 'Erro tratado: bad gateway cloudflare'."\n";
-				limpaCache($urlSearch,$host,$urlStart);
-				$countCacheInvalidado++;
-			}elseif(false !== strstr($contents2['content'], '502: Bad gateway</title>')){
-				echo 'Erro tratado: bad gateway'."\n";
-				limpaCache($urlSearch,$host,$urlStart);
-				$countCacheInvalidado++;
-			}else{
+			$errosBastaInvalidarCache = array();
+			$errosBastaInvalidarCache[] = (false !== strstr($contents2['content'], '<p>Please try again in a few minutes.</p>'));
+			$errosBastaInvalidarCache[] = (false !== strstr($contents2['content'], 'Could not resolve host: www.aosfatos.org'));
+			$errosBastaInvalidarCache[] = (isset($contents2['errmsg']) && false !== strstr($contents2['errmsg'], 'Could not resolve host'));
+			$errosBastaInvalidarCache[] = (isset($contents2['errmsg']) && false !== strstr($contents2['errmsg'], 'handshake'));
+			$errosBastaInvalidarCache[] = (false !== strstr($contents2['content'], 'Web server is returning an unknown error</title>'));
+			$errosBastaInvalidarCache[] = (false !== strstr($contents2['content'], '502: Bad gateway</title>'));
+			
+			//se não tratou desse erro
+			if (false === in_array(true, $errosBastaInvalidarCache)) {
 				echo 'Erro nao tratado:'."\n";
 				var_dump(str_replace("\n",'',$contents2['content']));
 				die();
 			}
+
+			limpaCache($urlSearch,$host,$urlStart);
+			$countCacheInvalidado++;
 
 		}
 	}
@@ -128,28 +103,21 @@ while(true){
 		break;
 	}
 
-	$page++;
 }
 
-echo 'Páginas processadas: '.$countPageResults."\n";
+echo 'Páginas processadas: '.$countPageResults."\n\n";
 
 //caso tenha invalidado algum cache (as vezes uma página de erro do servidor, algo assim...)
 if($countCacheInvalidado > 0){
-	//precisa rodar de novo
 	echo $countCacheInvalidado.' arquivos de cache foram invalidados.'."\n".'Rode novamente o script.'."\n";
 	die();
 }
-echo "\n";
-echo '---------------------------------------';
-echo "\n";
-
-//saída dos emojis
 
 //deixa em ordem cronológica crescente (do mais antigo para o mais recente)
 $arrOutput = array_reverse($arrOutput);
 
 //guarda menor e maior timestamps
-$menor = 999999999999999999;
+$menor = 999999999999999;
 $maior = 0;
 foreach($arrOutput as $arrItem){
     if($arrItem['timestamp'] < $menor){
@@ -160,7 +128,7 @@ foreach($arrOutput as $arrItem){
     }
 }
 
-//popula um array de dias com contagem de quantas mentiras naquele dia
+//popula um array de dias com contagem de quantas mentiras em cada dia
 $umDiaEmSegundos = 24 * 60 * 60;
 $maiorDia = 0;
 $arrDias = array();
@@ -181,42 +149,39 @@ for($c=0;$c<$maiorDia;$c++){
         $arrDias[$c] = 0;
     }
 }
-//acerta os índices
+//corrige os índices
 ksort($arrDias);
 
 //total de mentiras
-$mentirasTotal = 0;
-foreach($arrDias as $count){
-	$mentirasTotal += $count;
-}
+$mentirasTotal = array_sum($arrDias);
 
 //output inicial
-echo "\n";
 echo "De:  ".date('d/m/Y',$menor)."\n";
-echo "Até: ".date('d/m/Y',$maior)."\n";
-echo "\n";
-echo $mentirasTotal." mentiras ou distorções.";
-echo "\n";
-echo "\n";
+echo "Até: ".date('d/m/Y',$maior)."\n\n";
+echo $mentirasTotal." mentiras ou distorções.\n\n";
 echo "legenda:\n";
-echo 'Bolsonaro mentiu: '.$emojiMentiu."\n";
-echo 'não mentiu: '.$emojiOk."\n";
+echo 'Dias em que Bolsonaro mentiu ou distorceu informação publicamente: '.$emojiMentiu."\n";
+echo 'Dias em que não fez isto: '.$emojiOk."\n\n";
 
 //output dos emojis
+$numeroDiaDaSemanaQueComeca = intval(date('N', $menor));
+
+for($c=0;$c<$numeroDiaDaSemanaQueComeca;$c++){
+	echo '  ';
+}
+
 foreach($arrDias as $index => $quantas){
+	$index += $numeroDiaDaSemanaQueComeca;
     if($quantas == 0){
         echo $emojiOk;
     }else{
         echo $emojiMentiu;
     }
-    if($index % $colunas == $colunas-1){
-        echo "\n";
-    }
+	if($index % $colunas == $colunas - 1){
+		echo "\n";
+	}
+
 }
 
 //output da fonte
-echo "\n";
-echo "\n";
-echo 'Dados: https://www.aosfatos.org/todas-as-declara%C3%A7%C3%B5es-de-bolsonaro/';
-echo "\n";
-echo "\n";
+echo "\n\nDados: https://www.aosfatos.org/todas-as-declara%C3%A7%C3%B5es-de-bolsonaro/\n\n";
